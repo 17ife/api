8aa8890284178f0fb5794552ab782f3143712c1d
CarlosRen <419903711@qq.com>
2018-04-25 17:43:54 +0800

    save

--- config/config.default.js
+++ config/config.default.js
@@ -31,6 +31,7 @@ module.exports = appInfo => {
 
   config.security = {
     csrf: {
+      enable: false,
       queryName: '_csrf', // 通过 query 传递 CSRF token 的默认字段为 _csrf
       bodyName: '_csrf', // 通过 body 传递 CSRF token 的默认字段为 _csrf
     },
