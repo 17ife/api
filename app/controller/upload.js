3c84cfc141904ff1a28a2b72c6a531c4ee833fe2
CarlosRen <419903711@qq.com>
2018-05-22 17:42:01 +0800

    save

--- app/controller/upload.js
+++ app/controller/upload.js
@@ -1,5 +1,6 @@
 'use strict';
 const path             = require('path');
+const FormStream       = require('formstream');
 const sendToWormhole   = require('stream-wormhole');
 const Controller       = require('egg').Controller;
 const FileService      = require('../base/files');
@@ -8,25 +9,30 @@ const Fsrv             = new FileService();
 const Tool             = new ToolService();
 
 class UploadController extends Controller {
-  async uploadSingleFileToServer() {
-    const ctx = this.ctx;
-    const stream = await ctx.getFileStream();
-    const filePath = "/home/files/" + path.basename(stream.filename);
+  // async uploadSingleFileToServer() {
+  //   const ctx = this.ctx;
+  //   const stream = await ctx.getFileStream();
+  //   const filePath = "/home/files/" + path.basename(stream.filename);
 
-    try {
-      let r = await Fsrv.writeData(filePath,stream);
-    } catch (err) {
-      await sendToWormhole(stream);
-      throw err;
-    }
+  //   try {
+  //     let r = await Fsrv.writeData(filePath,stream);
+  //   } catch (err) {
+  //     await sendToWormhole(stream);
+  //     throw err;
+  //   }
     
-    if(r){
-      ctx.body = r
-    }else{
-      ctx.body = Tool.formatReturnValue(false,err,null);
-    }
-    
+  //   if(r){
+  //     ctx.body = r
+  //   }else{
+  //     ctx.body = Tool.formatReturnValue(false,err,null);
+  //   }
+  // }
+  async upload() {
+    const ctx   = this.ctx;
+    const form  = new FormStream();
   }
+
+
 }
 
 module.exports = UploadController;
