68dd35909e62feffa34abff93ad27d62725f3787
unknown <renchao3@cofco.com>
2018-07-31 16:06:11 +0800

    save

--- extends/wechatCypt/crypt.py
+++ extends/wechatCypt/crypt.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
     ret,sEchoStr        = wxcpt.VerifyURL(sVerifyMsgSig, sVerifyTimeStamp,sVerifyNonce,sVerifyEchoStr)
     if(ret!=0):
         # print "ERR: VerifyURL ret: " + str(ret)
-        sys.stderr(str(ret))
+        sys.stderr.write(str(ret))
         sys.exit(1)
 
-    sys.stdout(str(sEchoStr))
+    sys.stdout.write(str(sEchoStr))
     sys.exit(0)
     # print sEchoStr
     # return sEchoStr
