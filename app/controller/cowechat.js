a4b2558a3edb8ee693d6cad53dc391d6b6f60a88
unknown <renchao3@cofco.com>
2018-08-06 15:59:03 +0800

    save

--- app/controller/cowechat.js
+++ app/controller/cowechat.js
@@ -1,20 +1,19 @@
 'use strict';
 
-const wechat = require('co-wechat');
+const wechat     = require('co-wechat');
+const Controller = require('egg').Controller;
 
-module.exports = app => {
-  class cowechatController extends app.Controller {}
+class cowechatController extends Controller {}
 
-  // 因为 Egg 需要用类的形式来组织，而 wechat 是通过 middleware 方法来生成中间件
-  cowechatController.prototype.wechat = wechat({
-    token: 'wYA10XSUEzL1EQmdCyXFd9hzGNsM',
-    appid: 'wweeb673ca4f4dda8c',
-    encodingAESKey: 'iuSFA11uMgA86sTS2ZiadHxMtQjvpk6BzUSsnSQ9NiY'
-  }).middleware(async (message, ctx) => {
-    // TODO
-    console.log(message);
-    ctx.body = "ok"
-  });
+// 因为 Egg 需要用类的形式来组织，而 wechat 是通过 middleware 方法来生成中间件
+cowechatController.prototype.wechat = wechat({
+  token: 'wYA10XSUEzL1EQmdCyXFd9hzGNsM',
+  appid: 'wweeb673ca4f4dda8c',
+  encodingAESKey: 'iuSFA11uMgA86sTS2ZiadHxMtQjvpk6BzUSsnSQ9NiY'
+}).middleware(async (message, ctx) => {
+  // TODO
+  console.log(message);
+  ctx.body = "ok"
+});
 
-  return cowechatController;
-};
+module.exports = cowechatController;
