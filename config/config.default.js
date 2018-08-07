be6ea0b69bb1d7432ffaee47809a3f73c24ee377
unknown <renchao3@cofco.com>
2018-08-07 13:04:25 +0800

    save

--- config/config.default.js
+++ config/config.default.js
@@ -7,7 +7,7 @@ module.exports = appInfo => {
   config.keys = appInfo.name + '_1521705392823_8554';
 
   // add your config here
-  config.middleware = [ 'xmlparser' ],
+  config.middleware = [ 'xmlparse' ],
 
   config.bodyParser = {
     enable: true,
