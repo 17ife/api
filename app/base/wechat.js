e528da31a5486ec75a66f4ff50c00ec3998fe45c
carlos <419903711@qq.com>
2018-10-29 11:03:04 +0800

    save

--- app/base/wechat.js
+++ app/base/wechat.js
@@ -2,14 +2,20 @@
 
 const contactSercetID  =  "1A9PvBYbnLs3XnbTOaZT7Cygauc-7j2RM3ezgmD04-Y";
 const wechatURL        =  "https://qyapi.weixin.qq.com/cgi-bin/gettoken";
+const request          =  require("requestretry");
 
-
-class Wechat {
+class WechatBase {
   constructor(params) {
     contactSercetID = contactSercetID;
   }
 
-  async getAccessToken() {
-    
+  getAccessToken() {
+    return request({
+      url           : wechatURL,
+      json          : true,
+      fullResponse  : true
+    });
   }
 }
+
+module.exports = WechatBase;
